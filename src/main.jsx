e4438e5d9de945c1bb5d73eff0c7e14188129da7
Oleksandr <olexandr.veseluy@gmail.com>
2024-06-18 14:23:01 +0300

    update task

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import Modal from "react-modal";
+import "modern-normalize";
 
 import "./index.css";
 import App from "./components/App/App";
